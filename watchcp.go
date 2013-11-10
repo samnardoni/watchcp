442b594e66e4220d28819aba7246ec0919081a8e
Sam Nardoni <samnardoni@gmail.com>
2013-11-10 18:47:07 +0000

    cleanup

--- watchcp.go
+++ watchcp.go
@@ -4,9 +4,50 @@ import (
 	"flag"
 	"io"
 	"os"
-	"path/filepath"
 	"time"
 )
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 2 {
+		println("Usage: watchcp src dst")
+		return
+	}
+
+	src := flag.Arg(0)
+	dst := flag.Arg(1)
+
+	for {
+		copyFileIfNewer(src, dst)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func copyFileIfNewer(src, dst string) {
+	if shouldCopy(src, dst) {
+		println("Copying", src, "to", dst, "...")
+
+		_, err := copyFile(src, dst)
+		if err != nil {
+			println("Error copying file:", err.Error())
+		}
+	}
+}
+
+func shouldCopy(src, dst string) bool {
+	srcStat, err := os.Stat(src)
+	if err != nil {
+		return false
+	}
+
+	dstStat, err := os.Stat(dst)
+	if err != nil {
+		return true
+	}
+
+	return srcStat.ModTime().After(dstStat.ModTime())
+}
 
 func copyFile(src, dst string) (int64, error) {
 	sf, err := os.OpenFile(src, os.O_RDONLY, os.ModePerm)
@@ -22,66 +63,4 @@ func copyFile(src, dst string) (int64, error) {
 	}
 
 	return io.Copy(df, sf)
-}
-
-func instantTicker() chan time.Time {
-	c := make(chan time.Time)
-	go func() {
-		c <- time.Now()
-		t := time.NewTicker(1 * time.Second)
-		for x := range t.C {
-			c <- x
-		}
-	}()
-	return c
-}
-
-func waitStat(file string) time.Time {
-	for _ = range instantTicker() {
-
-		stat, err := os.Stat(file)
-		if err != nil {
-			continue
-		}
-
-		return stat.ModTime()
-	}
-
-	return time.Time{}
-}
-
-func main() {
-	flag.Parse()
-
-	if len(flag.Args()) != 2 {
-		println("Usage: watchcp src dst")
-		return
-	}
-
-	src := flag.Arg(0)
-	dst := flag.Arg(1)
-
-	for {
-		// Wait until src file becomes available
-		srcStat := waitStat(src)
-
-		// Wait until dst directory becomes available
-		waitStat(filepath.Dir(dst))
-
-		// Copy if dst file does not exist or src is newer then dst
-		dstStat, err := os.Stat(dst)
-
-		shouldCopy := err != nil || dstStat.ModTime().Before(srcStat)
-
-		if !shouldCopy {
-			continue
-		}
-
-		println("Copying", src, "to", dst, "...")
-
-		_, err = copyFile(src, dst)
-		if err != nil {
-			println("Error", err.Error())
-		}
-	}
 }
